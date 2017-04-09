7d26ca8e4e2bbfa7156a5fedb5fea172fc54efec
Sebastian Schmidt <publicarray@icloud.com>
2017-04-09 15:09:33 +1000

    archey on login shell only

--- conf.d/archey.fish
+++ conf.d/archey.fish
@@ -1,5 +1,7 @@
-if command -v archey >/dev/null
-   archey --offline
-else
-   uptime
+if status --is-login
+  if command -v archey >/dev/null
+     archey --offline
+  else
+     uptime
+  end
 end
