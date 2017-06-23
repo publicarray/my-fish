fecc3a7994bcd56555a229d10a9aa22e8cbad675
Sebastian Schmidt <publicarray@icloud.com>
2017-06-23 20:50:38 +1000

    code review

--- functions/vid2gif.fish
+++ functions/vid2gif.fish
@@ -9,50 +9,23 @@ function vid2gif -d "Convert video to gif file with ffmpeg"
       return 1
     end
 
-    set -l filename
+    set -l filename argv[1]
     set -l scale -1
     set -l fps 15
 
-    for i in (seq 1 (count $argv))
-      echo $argv[$i]
-      switch $i;
-        case 1;
-          set filename $argv[$i]
-        case 2;
-          set scale $argv[$i]
-        case 3;
-          set fps $argv[$i]
-        case '*'
-          echo "Ignored argument: $argv[$i]"
-      end
+    if set -q argv[2]
+      set scale $argv[2]
     end
 
-    # set -l i 1
-    # for arg in $argv;
-    #   echo "$i:$arg"
+    if set -q argv[3]
+      set fps $argv[3]
+    end
 
-    #   switch $i;
-    #     case 1;
-    #       set filename $arg
-    #     case 2;
-    #       set scale $arg
-    #     case 3;
-    #       set fps $arg
-    #     case '*'
-    #       echo "err"
-    #   end
-
-    #   set i (math $i+1)
-    # end
-
-    echo "$filename:$scale:$fps"
     ffmpeg -y -i "$filename" -vf fps=$fps,scale=$scale:-1:flags=lanczos,palettegen "$filename".png
-    # ffmpeg -y -i "${1}" -vf fps=${3:-10},scale=${2:-320}:-1:flags=lanczos,palettegen "${1}.png"
     ffmpeg -i $filename -i $filename.png -filter_complex "fps=$fps,scale=$scale:-1:flags=lanczos[x];[x][1:v]paletteuse" "$filename".gif
-    # ffmpeg -i "${1}" -i "${1}.png" -filter_complex "fps=${3:-10},scale=${2:-320}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${1}".gif
     rm -f "$filename".png
   else
-    echo "require ffmpeg\n install with: brew install ffmpeg/apt install ffmpeg/yum install ffmpeg"
+    printf "This script requires ffmpeg\nInstall ffmpeg with your package manager:\n    macOS: brew install ffmpeg\n    Linux: https://www.ffmpeg.org/download.html#build-linux\n    Windows: http://ffmpeg.zeranoe.com/builds/"
     return 1
   end
 end
