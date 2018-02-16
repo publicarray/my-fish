629feefb34cec79e92b628a29089f3811b37bfa6
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-02-16 19:26:54 +1000

    disable archey

--- conf.d/archey.fish
+++ conf.d/archey.fish
@@ -1,7 +1,7 @@
-if status --is-login
-  if command -v archey >/dev/null
-     archey --offline
-  else
-     uptime
-  end
-end
+# if status --is-login
+#   if command -v archey >/dev/null
+#      archey --offline
+#   else
+#      uptime
+#   end
+# end
