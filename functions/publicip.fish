41e4b1f0c0d87dd673c243d9f794e9b5cc175aac
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-08-13 19:37:30 +1000

    possible fallbacks

--- functions/publicip.fish
+++ functions/publicip.fish
@@ -1,5 +1,10 @@
 function publicip
   if not command dig +short myip.opendns.com @resolver1.opendns.com
     curl -qs https://checkip.amazonaws.com/
+    # curl -qs https://icanhazip.com
+    #  curl -qs https://ipv4.icanhazip.com
+    #  curl -qs https://ipv6.icanhazip.com
+    # curl -qs https://ipinfo.io/ip
+    # curl -qs checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'
   end
 end
