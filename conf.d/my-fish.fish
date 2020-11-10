16143de0d8851eee4e693d126c8edfe3dc0c20b4
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-11-11 10:57:18 +1100

    remove leftover debug statement

--- conf.d/my-fish.fish
+++ conf.d/my-fish.fish
@@ -2,7 +2,6 @@ set myfish_pkg oh-my-fish/plugin-hash oh-my-fish/plugin-osx oh-my-fish/plugin-ph
 
 function my-fish_install -e my-fish_install
   echo "Installing packages ..."
-  set -S myfish_pkg
   for pkg in $myfish_pkg
     fisher install "$pkg"
   end
