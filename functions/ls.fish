d28b611e6d800d3b26414ff1d77127eca6565260
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-12-13 13:04:31 +1100

    add ls -> lsd alias https://github.com/peltoche/lsd

--- functions/ls.fish
+++ functions/ls.fish
@@ -2,4 +2,8 @@ if command -sq exa
     function ls --description 'alias ls exa'
         exa $argv;
     end
+else if command -sq lsd
+    function ls --description 'alias ls lsd'
+        lsd $argv;
+    end
 end
