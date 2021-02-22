1b4422660f81bdaaa027115543d11574a218b92c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-02-22 20:03:23 +1100

    use aurutils instead

--- functions/aur.fish
+++
@@ -1,9 +0,0 @@
-function aur --description 'alias to a AUR helper/pacman wrapper'
-  if command -sq aur
-    # fallback if command already exists
-    aur $argv;
-  else
-    # see pacm.fish
-    pacm $argv;
-  end
-end
