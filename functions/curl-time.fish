9ce1081b2952aead0eddbe6227b5669a557f2167
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-03-04 19:41:53 +1100

    Add curl-time

---
+++ functions/curl-time.fish
@@ -0,0 +1,12 @@
+function curl-time -d "curl with timing statistics"
+# https://curl.se/docs/manpage.html
+    curl -so /dev/null -w "\
+   namelookup:  %{time_namelookup}s\n\
+      connect:  %{time_connect}s\n\
+   appconnect:  %{time_appconnect}s\n\
+  pretransfer:  %{time_pretransfer}s\n\
+     redirect:  %{time_redirect}s\n\
+starttransfer:  %{time_starttransfer}s\n\
+-------------------------\n\
+        total:  %{time_total}s\n" $argv
+end
