c24f62322a6387aef67c2f2b0ff418ba55c7087c
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-03-10 08:41:33 +1100

    fix sha256sum

--- functions/sha256sum.fish
+++ functions/sha256sum.fish
@@ -1,6 +1,6 @@
-# Defined in - @ line 1
-function sha256sum --description 'alias sha256sum shasum --algorithm 256'
-    if ! command -sq sha256sum
+if ! command -sq sha256sum
+    # for macOS compatability
+    function sha256sum --description 'alias sha256sum shasum --algorithm 256'
         shasum --algorithm 256 $argv
     end
 end
