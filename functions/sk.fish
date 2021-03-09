46ec9cfd81048b2c8c4569f13927720882dfaef8
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-03-10 08:53:12 +1100

    Add sk fuzzy finder

---
+++ functions/sk.fish
@@ -0,0 +1,6 @@
+if command -sq sk
+    function sk --description 'Fuzzy Finder in rust! - cargo install skim'
+        set SKIM_DEFAULT_COMMAND "fd --type f || git ls-tree -r --name-only HEAD || rg --files || find ."
+        sk --reverse --margin 1 --border=rounded $argv
+    end
+end
