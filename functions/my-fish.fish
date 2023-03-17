e64e79214b26ad5bdcd53f6ca410240cda49347f
Sebastian Schmidt <publicarray@users.noreply.github.com>
2023-03-18 09:39:11 +1100

    Update my-fish.fish

--- functions/my-fish.fish
+++ functions/my-fish.fish
@@ -17,7 +17,7 @@ function my-fish
 
     echo "Set Environment Variables"
     # remove fish greeting
-    set fish_greeting
+    set -U fish_greeting
     if command -sq brew
         set -Ux HOMEBREW_NO_ANALYTICS 1
     end
