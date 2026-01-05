93dbc7b6fa3b2d0227c4a1d51f22385310de7e84
Sebastian Schmidt <publicarray@users.noreply.github.com>
2026-01-05 23:12:06 +1100

    comment out shell colour, moved to theme file
    Comment out shell color settings for clarity.

--- functions/my-fish.fish
+++ functions/my-fish.fish
@@ -35,30 +35,30 @@ function my-fish
         ln -s /Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge /usr/local/bin
     end
 
-    echo "Set shell colours"
-    set -U fish_color_normal normal # the default color
-    set -U fish_color_command green # the color for commands
-    set -U fish_color_quote yellow # the color for quoted blocks of text
-    set -U fish_color_redirection normal # the color for IO redirections (|><)
-    set -U fish_color_end normal # the color for process separators like ';' and '&'
-    set -U fish_color_error red --bold # the color used to highlight potential errors
-    set -U fish_color_param normal # the color for regular command parameters
-    set -U fish_color_comment 555 # the color used for code comments
-    set -U fish_color_match purple --background=303030 # the color used to highlight matching parenthesis
-    set -U fish_color_search_match --background=0A0682 # the color used to highlight history search matches
-    set -U fish_color_operator normal # the color for parameter expansion operators like '*' and '~'
-    set -U fish_color_escape cyan # the color used to highlight character escapes like '\n' and '\x70'
-    set -U fish_color_cwd blue # the color used for the current working directory in the default prompt
-    set -U fish_color_cwd_root red
-    set -U fish_color_autosuggestion 555 # the color used for autosuggestions
-    set -U fish_color_user brgreen # the color used to print the current username in some of fish default prompts
-    set -U fish_color_host normal # the color used to print the current host system in some of fish default prompts
-    set -U fish_color_valid_path --underline
-    set -U fish_pager_color_prefix red # the color of the prefix string, i.e. the string that is to be completed
-    set -U fish_pager_color_completion normal # the color of the completion itself
-    set -U fish_pager_color_description 777 yellow # the color of the completion description
-    set -U fish_pager_color_progress cyan # the color of the progress bar at the bottom left corner
-    set -U fish_pager_color_secondary cyan # the background color of the every second completion
+    #echo "Set shell colours"
+    #set -U fish_color_normal normal # the default color
+    #set -U fish_color_command green # the color for commands
+    #set -U fish_color_quote yellow # the color for quoted blocks of text
+    #set -U fish_color_redirection normal # the color for IO redirections (|><)
+    #set -U fish_color_end normal # the color for process separators like ';' and '&'
+    #set -U fish_color_error red --bold # the color used to highlight potential errors
+    #set -U fish_color_param normal # the color for regular command parameters
+    #set -U fish_color_comment 555 # the color used for code comments
+    #set -U fish_color_match purple --background=303030 # the color used to highlight matching parenthesis
+    #set -U fish_color_search_match --background=0A0682 # the color used to highlight history search matches
+    #set -U fish_color_operator normal # the color for parameter expansion operators like '*' and '~'
+    #set -U fish_color_escape cyan # the color used to highlight character escapes like '\n' and '\x70'
+    #set -U fish_color_cwd blue # the color used for the current working directory in the default prompt
+    #set -U fish_color_cwd_root red
+    #set -U fish_color_autosuggestion 555 # the color used for autosuggestions
+    #set -U fish_color_user brgreen # the color used to print the current username in some of fish default prompts
+    #set -U fish_color_host normal # the color used to print the current host system in some of fish default prompts
+    #set -U fish_color_valid_path --underline
+    #set -U fish_pager_color_prefix red # the color of the prefix string, i.e. the string that is to be completed
+    #set -U fish_pager_color_completion normal # the color of the completion itself
+    #set -U fish_pager_color_description 777 yellow # the color of the completion description
+    #set -U fish_pager_color_progress cyan # the color of the progress bar at the bottom left corner
+    #set -U fish_pager_color_secondary cyan # the background color of the every second completion
     # colors for Man Pages
     set -Ux LESS_TERMCAP_mb \e'[01;31m' # begin blinking
     set -Ux LESS_TERMCAP_md \e'[01;31m' # begin bold
