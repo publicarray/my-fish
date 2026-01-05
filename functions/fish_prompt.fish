24f2f39e0ae859c504b6b7b4d75c07ab2069efbe
Sebastian Schmidt <publicarray@users.noreply.github.com>
2026-01-06 09:19:48 +1100

    Refactor fish_prompt to use STARSHIP_KEYMAP

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,11 +2,11 @@ if command -sq starship
     function fish_prompt
         switch "$fish_key_bindings"
             case fish_hybrid_key_bindings fish_vi_key_bindings
-                set keymap "$fish_bind_mode"
+                set STARSHIP_KEYMAP "$fish_bind_mode"
             case '*'
-                set keymap insert
+                set STARSHIP_KEYMAP insert
         end
-        starship prompt --status=$status --keymap=$keymap --cmd-duration=(math --scale=0 "$CMD_DURATION") --jobs=(count (jobs -p))
+        starship prompt --status=$status --pipestatus=$pipestatus --keymap=$STARSHIP_KEYMAP --cmd-duration=(math --scale=0 "$CMD_DURATION") --jobs=(count (jobs -p))
     end
 
     # disable virtualenv prompt, it breaks starship
@@ -16,5 +16,6 @@ if command -sq starship
     export STARSHIP_SHELL="fish"
 
     # Set up the session key that will be used to store logs
-    # export STARSHIP_SESSION_KEY=("/usr/local/bin/starship" session)
+    # We don't use `random [min] [max]` because it is unavailable in older versions of fish shell
+    #set -gx STARSHIP_SESSION_KEY (string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
 end
