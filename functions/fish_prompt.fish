cc1edeb35ca90019c46c917262b7ede435f9c388
Sebastian Schmidt <publicarray@users.noreply.github.com>
2026-01-06 09:30:45 +1100

    Update fish_prompt.fish from upstream

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -9,13 +9,15 @@ if command -sq starship
         starship prompt --status=$status --pipestatus=$pipestatus --keymap=$STARSHIP_KEYMAP --cmd-duration=(math --scale=0 "$CMD_DURATION") --jobs=(count (jobs -p))
     end
 
-    # disable virtualenv prompt, it breaks starship
-    set VIRTUAL_ENV_DISABLE_PROMPT 1
+    # Disable virtualenv prompt, it breaks starship
+    set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 
-    function fish_mode_prompt; end
-    export STARSHIP_SHELL="fish"
+    # Remove default mode prompt
+    builtin functions -e fish_mode_prompt
+
+    set -gx STARSHIP_SHELL "fish"
 
     # Set up the session key that will be used to store logs
     # We don't use `random [min] [max]` because it is unavailable in older versions of fish shell
-    #set -gx STARSHIP_SESSION_KEY (string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
+    set -gx STARSHIP_SESSION_KEY (string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
 end
