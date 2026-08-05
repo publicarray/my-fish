0a91f5b7a09db1dfc1c4ea88b47cf85b31d63ed4
publicarray <publicarray@users.noreply.github.com>
2026-08-05 19:42:50 +1000

    Enable icons in ls and richer long view in ll

--- functions/ll.fish
+++ functions/ll.fish
@@ -1,3 +1,11 @@
-function ll
-    ls -lH $argv
+function ll --description 'long listing, directories first'
+    # --git and --smart-group are backend specific, so mirror the detection order in ls.fish
+    if command -sq eza
+        ls -l --group-directories-first --git --smart-group $argv
+    else if command -sq lsd; or command -sq exa
+        # lsd and exa know --git but not --smart-group
+        ls -l --group-directories-first --git $argv
+    else
+        ls -l --group-directories-first $argv
+    end
 end
--- functions/ls.fish
+++ functions/ls.fish
@@ -1,16 +1,17 @@
 if command -sq eza
     function ls --description 'alias ls eza'
         # https://github.com/eza-community/eza
-        eza $argv
+        # icons=auto so they are dropped when output is piped
+        eza --icons=auto $argv
     end
 else if command -sq lsd
     function ls --description 'alias ls lsd'
         # https://github.com/lsd-rs/lsd
-        lsd $argv
+        lsd --icon=auto $argv
     end
 else if command -sq exa
     function ls --description 'alias ls exa'
         # https://github.com/ogham/exa (unmaintained, superseded by eza)
-        exa $argv
+        exa --icons $argv
     end
 end
