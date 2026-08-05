9936aefaebf8679d9205b0d3e6a0b448f52df413
publicarray <publicarray@users.noreply.github.com>
2026-08-05 18:52:28 +1000

    Update ls to prefer eza over unmaintained exa

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,11 +1,16 @@
-if command -sq exa
-    function ls --description 'alias ls exa'
-        # https://github.com/ogham/exa
-        exa $argv
+if command -sq eza
+    function ls --description 'alias ls eza'
+        # https://github.com/eza-community/eza
+        eza $argv
     end
 else if command -sq lsd
     function ls --description 'alias ls lsd'
-        # https://github.com/Peltoche/lsd
+        # https://github.com/lsd-rs/lsd
         lsd $argv
+    end
+else if command -sq exa
+    function ls --description 'alias ls exa'
+        # https://github.com/ogham/exa (unmaintained, superseded by eza)
+        exa $argv
     end
 end
