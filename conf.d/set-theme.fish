3f24ac0ffbf98f26b582fae5e80a44c5e725d67a
publicarray <publicarray@users.noreply.github.com>
2026-08-22 10:44:55 +1000

    Improve shell launch perf by only setting theme on install/update
    you loose the theme change when the OS theme changes but a fisher update
    would change from light and dark mode. (how often do you change your OS
    theme vs launch a shell and need to wait a few ms for the shell to
    render?)

--- conf.d/set-theme.fish
+++ conf.d/set-theme.fish
@@ -1,1 +1,7 @@
-fish_config theme choose publicarray
+function _set-theme_install --on-event my-fish_install
+    fish_config theme choose publicarray
+end
+
+function _set-theme_update --on-event my-fish_update
+    fish_config theme choose publicarray
+end
